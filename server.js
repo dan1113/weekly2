36e4a3f5113baf608264ff4ee966a90f351519e3
dan1113 <dan_1113@naver.com>
2025-11-10 03:01:28 +0900

    Asd

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 /* -------------------- Config -------------------- */
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 const NODE_ENV = process.env.NODE_ENV || "development";
 const isProd = NODE_ENV === "production";
 
